5bca9df58f341e78cc244db1d852b2b70ec09ca6
Dániel Kántor <git@daniel-kantor.com>
2020-11-10 08:31:51 +0100

    chore: add some new eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,36 @@
 module.exports = {
-  ignorePatterns: ["node_modules/"],
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    "jest/globals": true,
-    "cypress/globals": true,
-  },
-  extends: ["eslint:recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    process: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-  },
-  plugins: ["svelte3", "jest", "cypress"],
-  overrides: [
-    {
-      files: ["**/*.svelte"],
-      processor: "svelte3/svelte3",
+    ignorePatterns: ["node_modules/"],
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        "jest/globals": true,
+        "cypress/globals": true,
     },
-  ],
-  rules: {
-    indent: ["error", 4],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-  },
+    extends: ["eslint:recommended"],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+        process: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+    plugins: ["svelte3", "jest", "cypress", "import", "unused-imports"],
+    overrides: [
+        {
+            files: ["**/*.svelte"],
+            processor: "svelte3/svelte3",
+        },
+    ],
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "never"],
+        "import/max-dependencies": ["error", { max: 10 }],
+        "import/no-unused-modules": "error",
+        "unused-imports/no-unused-imports": "error",
+    },
 }
